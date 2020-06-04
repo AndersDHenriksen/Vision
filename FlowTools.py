8efdb6ee7acf5cf7afd035efaf664429e18764d7
Anders Dahl Henriksen <anders.d.henriksen@gmail.com>
2020-06-04 13:03:42 +0200

    -Added QtTools -added timestamp and try_except_pass

--- FlowTools.py
+++ FlowTools.py
@@ -1,6 +1,22 @@
+import time
 import traceback
+import functools
+
+def get_timestamp(hms_seperator=':'):
+    return time.strftime(f"%Y-%m-%d %H{hms_seperator}%M{hms_seperator}%S", time.localtime())
 
 
 def exception_traceback(e):
     e_tb = "\n".join(traceback.format_tb(e.__traceback__) + [str(e)])
     return e_tb
+
+
+def try_except_pass(func):
+    @functools.wraps(func)
+    def wrapper_decorator(*args, **kwargs):
+        try:
+            value = func(*args, **kwargs)
+        except:
+            value = None
+        return value
+    return wrapper_decorator
---
+++ QtTools.py
@@ -0,0 +1,54 @@
+from PyQt5 import QtGui as qtg
+
+
+WHITE =     qtg.QColor(255, 255, 255)
+BLACK =     qtg.QColor(0, 0, 0)
+RED =       qtg.QColor(255, 0, 0)
+PRIMARY =   qtg.QColor(53, 53, 53)
+SECONDARY = qtg.QColor(35, 35, 35)
+TERTIARY =  qtg.QColor(42, 130, 218)
+GREY =      qtg.QColor(128, 128, 128)
+
+
+class QDarkPalette(qtg.QPalette):
+    """Dark palette for a Qt application meant to be used with the Fusion theme."""
+    def __init__(self, *__args, app=None):
+        super().__init__(*__args)
+
+        # Set all the colors based on the constants in globals
+        self.setColor(qtg.QPalette.Window,          PRIMARY)
+        self.setColor(qtg.QPalette.WindowText,      WHITE)
+        self.setColor(qtg.QPalette.Base,            SECONDARY)
+        self.setColor(qtg.QPalette.AlternateBase,   PRIMARY)
+        self.setColor(qtg.QPalette.ToolTipBase,     WHITE)
+        self.setColor(qtg.QPalette.ToolTipText,     WHITE)
+        self.setColor(qtg.QPalette.Text,            WHITE)
+        self.setColor(qtg.QPalette.Button,          PRIMARY)
+        self.setColor(qtg.QPalette.ButtonText,      WHITE)
+        self.setColor(qtg.QPalette.BrightText,      RED)
+        self.setColor(qtg.QPalette.Link,            TERTIARY)
+        self.setColor(qtg.QPalette.Highlight,       TERTIARY)
+        self.setColor(qtg.QPalette.HighlightedText, BLACK)
+        self.setColor(qtg.QPalette.Disabled, qtg.QPalette.Text,       GREY)
+        self.setColor(qtg.QPalette.Disabled, qtg.QPalette.ButtonText, GREY)
+        self.setColor(qtg.QPalette.Disabled, qtg.QPalette.WindowText, GREY)
+        self.setColor(qtg.QPalette.Disabled, qtg.QPalette.Button,     SECONDARY)
+
+        if app:
+            self.set_app(app)
+
+    @staticmethod
+    def set_stylesheet(app):
+        css_rgb = lambda color:  "rgb({}, {}, {})".format(*color.getRgb())
+        """Static method to set the tooltip stylesheet to a `QtWidgets.QApplication`."""
+        app.setStyleSheet("QToolTip {{"
+                          "color: {white};"
+                          "background-color: {tertiary};"
+                          "border: 1px solid {white};"
+                          "}}".format(white=css_rgb(WHITE), tertiary=css_rgb(TERTIARY)))
+
+    def set_app(self, app):
+        """Set the Fusion theme and this palette to a `QtWidgets.QApplication`."""
+        app.setStyle("Fusion")
+        app.setPalette(self)
+        self.set_stylesheet(app)
