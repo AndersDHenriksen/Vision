9dde1375e36e5436c1d99fb94232eeb393b00642
Anders Dahl Henriksen <ahe@proinvent.dk>
2021-04-15 15:41:01 +0200

    -Bugfix and improvement to shared memory

--- DecoratorTools.py
+++ DecoratorTools.py
@@ -45,7 +45,7 @@ def md5(input_string):
     return hashlib.md5(str(input_string).encode('utf8')).hexdigest()
 
 
-def path2image(load_from_shared_memory=False):
+def path2image(enforce_grayscale=False, load_from_shared_memory=False):
     from multiprocessing import shared_memory
     from PIL import Image
 
@@ -54,27 +54,31 @@ def path2image(load_from_shared_memory=False):
         def wrapper(*args, **kwargs):
             if isinstance(args[0], str) or isinstance(args[0], Path):
                 if load_from_shared_memory:
-                    existing_shm = shared_memory.SharedMemory(name=md5(args[0]))
-                    img_shape = Image.open(args[0]).size
-                    image = np.ndarray(img_shape, dtype=np.uint8, buffer=existing_shm.buf)
+                    try:
+                        existing_shm = shared_memory.SharedMemory(name=md5(args[0]))
+                        img_shape = Image.open(args[0]).size
+                        image = np.ndarray(img_shape[::-1], dtype=np.uint8, buffer=existing_shm.buf)
+                    except FileNotFoundError:
+                        print("Image could not be found in shared memory. Loading from disc.")
+                        image = cv2.imread(str(args[0]), cv2.IMREAD_GRAYSCALE if enforce_grayscale else -1)
                 else:
-                    image = cv2.imread(str(args[0]), -1)
+                    image = cv2.imread(str(args[0]), cv2.IMREAD_GRAYSCALE if enforce_grayscale else -1)
                 args = (image, *args[1:])
             return func(*args, **kwargs)
         return wrapper
     return decorator
 
 
-def put_images_into_shared_memory(img_dir, pattern):
+def put_images_into_shared_memory(img_dir, pattern="*.png", enforce_grayscale=False):
     from multiprocessing import shared_memory
     shms = []
     for img_path in Path(img_dir).rglob(pattern):
-        img = cv2.imread(str(img_path), -1)
+        img = cv2.imread(str(img_path), cv2.IMREAD_GRAYSCALE if enforce_grayscale else -1)
         shm = shared_memory.SharedMemory(name=md5(img_path), create=True, size=img.nbytes)
         shms.append(shm)
         img_shm = np.ndarray(img.shape, dtype=img.dtype, buffer=shm.buf)
         img_shm[:] = img[:]
-        print(f"Loaded: {img_path}")
+        print(f"Loaded {md5(img_path)}: {img_path}")
     input("Images stored in shared memory. Input enter to shutdown shared memory.")
     for shm in shms:
         shm.unlink()
@@ -82,4 +86,4 @@ def put_images_into_shared_memory(img_dir, pattern):
 
 if __name__ == "__main__":
     print(f"DecoratorTools called with argv: {sys.argv[1:]}")
-    put_images_into_shared_memory(sys.argv[1], sys.argv[2])
+    put_images_into_shared_memory(sys.argv[1], enforce_grayscale=True)
